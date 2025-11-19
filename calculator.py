72b46a2342b018b12ccb317f574dca0c7252608b
Sebastian Bernal <sebastbernal2@gmail.com>
2025-11-19 18:42:10 -0500

    added or updated calculator functions

--- calculator.py
+++ calculator.py
@@ -9,3 +9,27 @@ def add(a, b):
     pass
 
 
+import math
+
+def add(a, b): 
+    return a + b
+
+def subtract(a, b): 
+    return a - b
+
+def multiply(a, b): 
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        raise ZeroDivisionError("Division by zero!")
+    return a / b
+
+def logarithm(a, b):
+    if a <= 0 or a == 1 or b <= 0:
+        raise ValueError("Invalid base or argument for logarithm!")
+    return math.log(b, a)
+
+def exponent(a, b): 
+    return a**b
+
